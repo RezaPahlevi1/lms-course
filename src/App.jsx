099f332462fe3f8e2e9c11eabed729b270d510b2
RezaPahlevi1 <flashwatchreza@gmail.com>
2025-10-03 13:44:44 +0700

    feat: react and tailwind configuration

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1 className="p-10 text-4xl">THIS IS LMS COURSE</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
