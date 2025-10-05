caeab4c2da3c316d10c30ab9f474553497489feb
RezaPahlevi1 <flashwatchreza@gmail.com>
2025-10-05 13:32:52 +0700

    feat: rapiin struktur folder

--- src/pages/Reza.jsx
+++ src/pages/Reza.jsx
@@ -1,5 +1,35 @@
+import { ProgressBar } from "../components/ProgressBar";
+
 function Reza() {
-  return <div></div>;
+  return (
+    <div>
+      <CourseHero />
+      <Middle />
+    </div>
+  );
+}
+
+function CourseHero() {
+  return (
+    <div className="w-full text-white bg-linear-to-r from-black to-[#293352] flex flex-col p-12">
+      <h1 className="text-6xl">BLABLALBALBA</h1>
+      <div className="flex flex-row gap-5 pt-20">
+        <h1>awodnawodnwao</h1>
+        <h1>awodnawodnwao</h1>
+        <h1>awodnawodnwao</h1>
+      </div>
+    </div>
+  );
+}
+
+function Middle() {
+  return (
+    <div className="p-10 flex flex-col bg-gray-200">
+      <h1>WOOOOOOOOOOOOOOO</h1>
+      <h1>WOOOOOOOOOOOOOOO</h1>
+      <ProgressBar />
+    </div>
+  );
 }
 
 export default Reza;
