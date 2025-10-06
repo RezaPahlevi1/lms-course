01b119e74374e7771bd443798e60f9266adcc700
raihanph21 <m.rhn.ph@gmail.com>
2025-10-06 13:06:29 +0700

    fix: fixing CourseContent element

--- src/components/CourseContent.jsx
+++ src/components/CourseContent.jsx
@@ -18,12 +18,12 @@ export function CourseContent({ activeModule, setActiveModule, modules }) {
   console.log(modules.length);
 
   return (
-    <div className="p-15">
+    <div className="p-15 w-screen">
       <div className="flex justify-between mb-15">
         <p>Lesson 69 of 420</p>
         <button className="bg-[#A51C30] text-white rounded p-2">EXIT COURSE</button>
       </div>
-      <div className="bg-[#293352] text-white p-15 rounded-md w-350">
+      <div className="bg-[#293352] text-white p-15 rounded-md">
         <h1 className="text-center mb-20">{activeModule.title}</h1>
         {activeModule.type === "video" && <video src={activeModule.content.video_url} controls className="w-full mt-5" />}
         {activeModule.type === "article" && <div dangerouslySetInnerHTML={{ __html: activeModule.content.html }} />}
