979d3d43f43afa7abde1312fd5e246984008ebb6
raihanph21 <m.rhn.ph@gmail.com>
2025-10-06 17:19:29 +0700

    feat: moving code from Rehan.jsx to CourseModules.jsx

--- src/pages/Rehan.jsx
+++ src/pages/Rehan.jsx
@@ -12,24 +12,6 @@ function Rehan() {
   const currentCourse = courses.find((c) => c.id === courseId);
   const [activeModule, setActiveModule] = useState(courseModules[0] || null);
   const [completedModules, setCompletedModules] = useState([]);
-  // const [modules, setModules] = useState([]);
-  // const [courseTitle, setCourseTitle] = useState(null);
-  // const [activeModule, setActiveModule] = useState(null);
-
-  // const activeCourseId = "course_01"; // course aktif sekarang
-
-  // useEffect(() => {
-  //   fetch("/db.json")
-  //     .then((res) => res.json())berk
-  //     .then((data) => {
-  //       const courseModules = data.modules.filter((module) => module.courseId === activeCourseId).sort((a, b) => a.position - b.position);
-  //       const currentCourse = data.courses.find((course) => course.id === activeCourseId);
-  //       setModules(courseModules);
-  //       setActiveModule(courseModules[0]);
-  //       setCourseTitle(currentCourse ? currentCourse.title : "-");
-  //     })
-  //     .catch((err) => console.error("Failed to load db.json:", err));
-  // }, []);
 
   return (
     <div className="flex flex-row">
