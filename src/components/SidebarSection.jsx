57e8c3402d32a743e856009efe890bb315ea9ad3
raihanph21 <m.rhn.ph@gmail.com>
2025-10-06 17:01:33 +0700

    feat: using DataContext in rehan.jsx

--- src/components/SidebarSection.jsx
+++ src/components/SidebarSection.jsx
@@ -19,14 +19,6 @@ export function SidebarSection({ modules, setActiveModule, activeModule, complet
           </li>
         ))}
       </ul>
-      {/* <ul>
-        {modules &&
-          modules.map((mod) => (
-            <li key={mod.id} onClick={() => setActiveModule(mod)} className={`p-2 rounded ${activeModule?.id === mod.id ? "bg-[#A51C30]" : "hover:bg-[#3a4662]"}`}>
-              {mod.title}
-            </li>
-          ))}
-      </ul> */}
     </div>
   );
 }
--- src/pages/Rehan.jsx
+++ src/pages/Rehan.jsx
@@ -1,31 +1,40 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { Sidebar } from "../components/Sidebar";
 import { CourseContent } from "../components/CourseContent";
+import { useParams } from "react-router-dom";
+import { useData } from "../context/DataContext";
 
 function Rehan() {
-  const [modules, setModules] = useState([]);
-  const [courseTitle, setCourseTitle] = useState(null);
-  const [activeModule, setActiveModule] = useState(null);
-  const [completedModules, setCompletedModules] = useState([]);
-  const activeCourseId = "course_01"; // course aktif sekarang
+  const { courseId } = useParams();
+  const { courses, modules } = useData();
 
-  useEffect(() => {
-    fetch("/db.json")
-      .then((res) => res.json())
-      .then((data) => {
-        const courseModules = data.modules.filter((module) => module.courseId === activeCourseId).sort((a, b) => a.position - b.position);
-        const currentCourse = data.courses.find((course) => course.id === activeCourseId);
-        setModules(courseModules);
-        setActiveModule(courseModules[0]);
-        setCourseTitle(currentCourse ? currentCourse.title : "-");
-      })
-      .catch((err) => console.error("Failed to load db.json:", err));
-  }, []);
+  const courseModules = modules.filter((m) => m.courseId === courseId).sort((a, b) => a.position - b.position);
+  const currentCourse = courses.find((c) => c.id === courseId);
+  const [activeModule, setActiveModule] = useState(courseModules[0] || null);
+  const [completedModules, setCompletedModules] = useState([]);
+  // const [modules, setModules] = useState([]);
+  // const [courseTitle, setCourseTitle] = useState(null);
+  // const [activeModule, setActiveModule] = useState(null);
+
+  // const activeCourseId = "course_01"; // course aktif sekarang
+
+  // useEffect(() => {
+  //   fetch("/db.json")
+  //     .then((res) => res.json())berk
+  //     .then((data) => {
+  //       const courseModules = data.modules.filter((module) => module.courseId === activeCourseId).sort((a, b) => a.position - b.position);
+  //       const currentCourse = data.courses.find((course) => course.id === activeCourseId);
+  //       setModules(courseModules);
+  //       setActiveModule(courseModules[0]);
+  //       setCourseTitle(currentCourse ? currentCourse.title : "-");
+  //     })
+  //     .catch((err) => console.error("Failed to load db.json:", err));
+  // }, []);
 
   return (
     <div className="flex flex-row">
-      <Sidebar modules={modules} activeModule={activeModule} setActiveModule={setActiveModule} completedModules={completedModules} setCompletedModules={setCompletedModules} courseTitle={courseTitle} />
-      <CourseContent activeModule={activeModule} setActiveModule={setActiveModule} modules={modules} completedModules={completedModules} setCompletedModules={setCompletedModules} />
+      <Sidebar modules={courseModules} activeModule={activeModule} setActiveModule={setActiveModule} courseTitle={currentCourse?.title || "-"} completedModules={completedModules} setCompletedModules={setCompletedModules} />
+      <CourseContent activeModule={activeModule} setActiveModule={setActiveModule} modules={courseModules} completedModules={completedModules} setCompletedModules={setCompletedModules} />
     </div>
   );
 }
