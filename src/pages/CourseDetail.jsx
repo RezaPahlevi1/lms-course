cb4aed886c7c88b0e7d9112becf4d90dd7bec6fa
RezaPahlevi1 <flashwatchreza@gmail.com>
2025-10-06 16:16:06 +0700

    feat: course detail using reusable fetch data (still error)

--- src/pages/CourseDetail.jsx
+++ src/pages/CourseDetail.jsx
@@ -3,38 +3,39 @@ import { ProgressBar } from "../components/ProgressBar";
 import { FaClock } from "react-icons/fa";
 import { IoPerson } from "react-icons/io5";
 import { IoBook } from "react-icons/io5";
-import { FaStar } from "react-icons/fa";
 import { LittleDetail } from "../components/LittleDetail";
 import { Hero } from "../components/Hero";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
-import { CourseCard } from "../components/CourseCard";
+import { useData } from "../context/DataContext";
 
 export default function CourseDetail() {
   const { id } = useParams();
-  const [course, setCourse] = useState(null);
-  const [instructor, setInstructor] = useState(null);
-  const [modules, setModules] = useState([]);
+  const { courses, instructors, modules } = useData();
 
-  useEffect(() => {
-    fetch("/db.json")
-      .then((res) => res.json())
-      .then((data) => {
-        const foundCourse = data.courses.find((c) => c.id === id);
-        if (foundCourse) {
-          setCourse(foundCourse);
-          const foundInstructor = data.instructors.find(
-            (i) => i.id === foundCourse.instructorId
-          );
-          setInstructor(foundInstructor);
-          const relatedModules = data.modules.filter(
-            (m) => m.courseId === foundCourse.id
-          );
-          setModules(relatedModules);
-        }
-      })
-      .catch((err) => console.error("Failed to load db.json:", err));
-  }, [id]);
+  // useEffect(() => {
+  //   fetch("/db.json")
+  //     .then((res) => res.json())
+  //     .then((data) => {
+  //       const foundCourse = data.courses.find((c) => c.id === id);
+  //       if (foundCourse) {
+  //         setCourse(foundCourse);
+  //         const foundInstructor = data.instructors.find(
+  //           (i) => i.id === foundCourse.instructorId
+  //         );
+  //         setInstructor(foundInstructor);
+  //         const relatedModules = data.modules.filter(
+  //           (m) => m.courseId === foundCourse.id
+  //         );
+  //         setModules(relatedModules);
+  //       }
+  //     })
+  //     .catch((err) => console.error("Failed to load db.json:", err));
+  // }, [id]);
+  const navigate = useNavigate();
+  const course = courses.find((c) => c.id === id);
+  const instructor = instructors.find((i) => i.id === course?.instructorId);
+  const relatedModules = modules.filter((m) => m.courseId === course?.id);
 
   if (!course) return <p className="p-10 text-xl">Loading course...</p>;
 
@@ -54,15 +55,42 @@ export default function CourseDetail() {
             />
             <LittleDetail
               icon={<IoBook />}
-              detail={`${modules.length} Module`}
+              detail={`${relatedModules.length} Module`}
             />
           </div>
         </div>
       </Hero>
-      <Middle modules={modules} />
+      <Middle navigate={navigate} relatedModules={relatedModules} />
       <CourseDescription course={course} />
-      <CourseTag tags={course.tags} />
-      <CourseRating rating={course.rating} enroll_count={course.enroll_count} />
+    </div>
+  );
+}
+
+function CourseDescription({ course }) {
+  return (
+    <div className="flex flex-col gap-5 p-10">
+      <h1 className="font-bold text-4xl">About {course.title}</h1>
+      <p>{course.description}</p>
+    </div>
+  );
+}
+
+function Middle({ relatedModules, navigate }) {
+  return (
+    <div className="p-10 flex flex-col gap-4 bg-gray-300">
+      <div className="flex flex-row justify-between">
+        <button className="p-2 px-8 rounded-xl bg-[#293352] text-white cursor-pointer">
+          On Going
+        </button>
+        <button
+          onClick={() => navigate(`/module/${relatedModules[0].id}`)}
+          className="p-2 px-8 rounded-xl bg-[#293352] text-white cursor-pointer"
+        >
+          Learn Now
+        </button>
+      </div>
+      <ProgressBar />
+      <p>X out of {relatedModules.length} modules finished</p>
     </div>
   );
 }
@@ -129,32 +157,6 @@ function CourseTag({ tags = [] }) {
           </div>
         ))}
       </div>
-    </div>
-  );
-}
-
-function CourseDescription({ course }) {
-  return (
-    <div className="flex flex-col gap-5 p-10">
-      <h1 className="font-bold text-4xl">About {course.title}</h1>
-      <p>{course.description}</p>
-    </div>
-  );
-}
-
-function Middle({ modules }) {
-  return (
-    <div className="p-10 flex flex-col gap-4 bg-gray-300">
-      <div className="flex flex-row justify-between">
-        <button className="p-2 px-8 rounded-xl bg-[#293352] text-white cursor-pointer">
-          On Going
-        </button>
-        <button className="p-2 px-8 rounded-xl bg-[#293352] text-white cursor-pointer">
-          Learn Now
-        </button>
-      </div>
-      <ProgressBar />
-      <p>X out of {modules.length} modules finished</p>
     </div>
   );
 }
