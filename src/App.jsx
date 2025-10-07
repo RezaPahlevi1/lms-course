4132ddb1b883c22f37fc3a88e753e35ff6d60019
RezaPahlevi1 <flashwatchreza@gmail.com>
2025-10-07 14:19:39 +0700

    feat: make exceptional footer for module page

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,18 @@ import "./App.css";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 import CourseList from "./pages/CourseList";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 
 function App() {
+  const location = useLocation();
+  const hideFooter = location.pathname.includes("/modules");
   return (
     <>
       <Navbar />
       <main className="">
         <Outlet />
       </main>
-      <Footer />
+      {!hideFooter && <Footer />}
     </>
   );
 }
